fb38f5a1534abcdf37b0ab3de3802bf1c3f2ebcd
romain <romain.poitrenaud@gmail.com>
2023-12-06 10:26:25 +0100

    time update

--- FirstUnitTest.java
+++ FirstUnitTest.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
 import java.lang.*; 
+import java.util.Date;
+import java.time.LocalTime;
 
 
 public class FirstUnitTest {
 
     public static void main(String[] args) {
 
-        System.out.print("Boujour");
+        LocalTime now = LocalTime.now();
+        LocalTime midi = LocalTime.of(12, 00, 00);
+        LocalTime soir = LocalTime.of(18, 00, 00);
 
-        System.out.print("\n Entrer un mot a inverser: ");
+
+        if (now.isBefore(midi)) {
+            System.out.print("Bon matin");
+        } else if (now.isBefore(soir)) {
+            System.out.print("Bonjour");
+        }else{
+            System.out.print("Bonsoir");
+        }
+        
+
+        System.out.print("\nEntrer un mot a inverser: ");
 
         Scanner input = new Scanner(System.in);
 
         String mot = input.nextLine();
 
         StringBuilder palindrome = new StringBuilder(mot); 
-  
 
         mot = palindrome.reverse().toString();
 
         System.out.println(palindrome);
         
-        System.out.print("aurevoir");
+        if (now.isBefore(midi)) {
+            System.out.print("Bonne matinée");
+        } else if (now.isBefore(soir)) {
+            System.out.print("Bonne journée");
+        }else{
+            System.out.print("Bonne soirée");
+        }
 
     }
 }
